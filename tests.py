646cb53a6eb7253edb20b1cc0e0dd9f780c0e4d2
Caleb Evans <caleb@calebevans.me>
2016-03-31 19:47:28 -0700

    Switch from unittest to nose

--- tests.py
+++ tests.py
@@ -1,99 +1,107 @@
 #!/usr/bin/env python3
 
-import unittest
+import nose.tools as nose
 
 import cidrbrewer
 
 
-class TestCIDRBrewer(unittest.TestCase):
+def test_get_subnet_mask():
+    """Should compute the subnet mask."""
+    nose.assert_equal(
+        cidrbrewer.get_subnet_mask(28),
+        '11111111111111111111111111110000')
 
-    def test_get_subnet_mask(self):
-        """Should compute the subnet mask."""
-        self.assertEqual(
-            cidrbrewer.get_subnet_mask(28),
-            '11111111111111111111111111110000')
 
-    def test_get_network_id(self):
-        """Should compute the network ID of an IP address."""
-        self.assertEqual(cidrbrewer.get_network_id(
-            '11001000000101110001000001011100', 28),
-            '11001000000101110001000001010000')
+def test_get_network_id():
+    """Should compute the network ID of an IP address."""
+    nose.assert_equal(cidrbrewer.get_network_id(
+        '11001000000101110001000001011100', 28),
+        '11001000000101110001000001010000')
 
-    def test_get_broadcast_id(self):
-        """Should compute the broadcast ID of an IP address."""
-        self.assertEqual(cidrbrewer.get_broadcast_id(
-            '11001000000101110001000001011100', 28),
-            '11001000000101110001000001011111')
 
-    def test_get_first_available_addr(self):
-        """Should compute the first available IP address."""
-        self.assertEqual(cidrbrewer.get_first_available_addr(
-            '11001000000101110001000001011100', 28),
-            '11001000000101110001000001010001')
+def test_get_broadcast_id():
+    """Should compute the broadcast ID of an IP address."""
+    nose.assert_equal(cidrbrewer.get_broadcast_id(
+        '11001000000101110001000001011100', 28),
+        '11001000000101110001000001011111')
 
-    def test_get_last_available_addr(self):
-        """Should compute the last available IP address."""
-        self.assertEqual(cidrbrewer.get_last_available_addr(
-            '11001000000101110001000001011100', 28),
-            '11001000000101110001000001011110')
 
-    def test_prettify_bin_addr(self):
-        """Should prettify a binary IP address."""
-        self.assertEqual(cidrbrewer.prettify_bin_addr(
-            '11001000000101110001000001011100'),
-            '11001000.00010111.00010000.01011100')
+def test_get_first_available_addr():
+    """Should compute the first available IP address."""
+    nose.assert_equal(cidrbrewer.get_first_available_addr(
+        '11001000000101110001000001011100', 28),
+        '11001000000101110001000001010001')
 
-    def test_get_prettified_dec_addr(self):
-        """Should build a prettified decimal IP address"""
-        self.assertEqual(cidrbrewer.get_prettified_dec_addr(
-            '11001000000101110001000001011100'), '200.23.16.92')
 
-    def test_get_subnet_size(self):
-        """Should compute the size of the subnet."""
-        self.assertEqual(cidrbrewer.get_subnet_size(28), 14)
+def test_get_last_available_addr():
+    """Should compute the last available IP address."""
+    nose.assert_equal(cidrbrewer.get_last_available_addr(
+        '11001000000101110001000001011100', 28),
+        '11001000000101110001000001011110')
 
-    def test_is_reserved_addr(self):
-        """Should determine that an unreserved IP address is unreserved."""
-        self.assertEqual(cidrbrewer.is_reserved(
-            '11001000000101110001000001011100', 28), False)
 
-    def test_is_reserved_network_id(self):
-        """Should determine that a network ID is reserved"""
-        self.assertEqual(cidrbrewer.is_reserved(
-            '11001000000101110001000001010000', 28), True)
+def test_prettify_bin_addr():
+    """Should prettify a binary IP address."""
+    nose.assert_equal(cidrbrewer.prettify_bin_addr(
+        '11001000000101110001000001011100'),
+        '11001000.00010111.00010000.01011100')
 
-    def test_is_reserved_broadcast_id(self):
-        """Should determine that a broadcast ID is reserved"""
-        self.assertEqual(cidrbrewer.is_reserved(
-            '01111101001011110010000000101111', 28), True)
 
-    def test_get_largest_subnet_mask(self):
-        """Should compute the largest subnet mask for two IP addresses."""
-        self.assertEqual(cidrbrewer.get_largest_subnet_mask(
-            '01111101001011110010000000100001',
-            '01111101001011110010000000101111'),
-            '11111111111111111111111111100000')
+def test_get_prettified_dec_addr():
+    """Should build a prettified decimal IP address"""
+    nose.assert_equal(cidrbrewer.get_prettified_dec_addr(
+        '11001000000101110001000001011100'), '200.23.16.92')
 
-    def test_get_block_network_id(self):
-        """Should compute the network ID of a block"""
-        self.assertEqual(cidrbrewer.get_block_network_id(
-            '00010000001000111001110110000000',
-            num_subnet_bits=26,
-            block_size=16),
-            '00010000001000111001110110010000')
 
-    def test_get_blocks(self):
-        """Should compute the list of data for each block"""
-        self.assertEqual(cidrbrewer.get_blocks(
-            '00010000001000111001110110000000',
-            num_subnet_bits=26,
-            block_sizes=(16, 64, 16, 32)),
-            [
-                (64, '00010000001000111001110110000000', 26),
-                (32, '00010000001000111001110111000000', 27),
-                (16, '00010000001000111001110111100000', 28),
-                (16, '00010000001000111001110111110000', 28)
-            ])
+def test_get_subnet_size():
+    """Should compute the size of the subnet."""
+    nose.assert_equal(cidrbrewer.get_subnet_size(28), 14)
 
-if __name__ == '__main__':
-    unittest.main()
+
+def test_is_reserved_addr():
+    """Should determine that an unreserved IP address is unreserved."""
+    nose.assert_equal(cidrbrewer.is_reserved(
+        '11001000000101110001000001011100', 28), False)
+
+
+def test_is_reserved_network_id():
+    """Should determine that a network ID is reserved"""
+    nose.assert_equal(cidrbrewer.is_reserved(
+        '11001000000101110001000001010000', 28), True)
+
+
+def test_is_reserved_broadcast_id():
+    """Should determine that a broadcast ID is reserved"""
+    nose.assert_equal(cidrbrewer.is_reserved(
+        '01111101001011110010000000101111', 28), True)
+
+
+def test_get_largest_subnet_mask():
+    """Should compute the largest subnet mask for two IP addresses."""
+    nose.assert_equal(cidrbrewer.get_largest_subnet_mask(
+        '01111101001011110010000000100001',
+        '01111101001011110010000000101111'),
+        '11111111111111111111111111100000')
+
+
+def test_get_block_network_id():
+    """Should compute the network ID of a block"""
+    nose.assert_equal(cidrbrewer.get_block_network_id(
+        '00010000001000111001110110000000',
+        num_subnet_bits=26,
+        block_size=16),
+        '00010000001000111001110110010000')
+
+
+def test_get_blocks():
+    """Should compute the list of data for each block"""
+    nose.assert_equal(cidrbrewer.get_blocks(
+        '00010000001000111001110110000000',
+        num_subnet_bits=26,
+        block_sizes=(16, 64, 16, 32)),
+        [
+            (64, '00010000001000111001110110000000', 26),
+            (32, '00010000001000111001110111000000', 27),
+            (16, '00010000001000111001110111100000', 28),
+            (16, '00010000001000111001110111110000', 28)
+        ])
