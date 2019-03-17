2c3757ec58935bc386a5f874aa2549f3e0e678d5
Caleb Evans <caleb@calebevans.me>
2019-03-17 13:26:02 -0700

    Remove pypandoc since PyPI now supports Markdown

--- setup.py
+++ setup.py
@@ -6,12 +6,8 @@ from setuptools import setup
 
 # Get long description (used on PyPI project page)
 def get_long_description():
-    try:
-        # Use pandoc to create reStructuredText README if possible
-        import pypandoc
-        return pypandoc.convert_file('README.md', 'rst')
-    except Exception:
-        return None
+    with open('README.md', 'r') as readme_file:
+        return readme_file.read()
 
 
 setup(
@@ -19,6 +15,7 @@ setup(
     version='1.0.0',
     description='A CLI utility for working with classless IP addresses',
     long_description=get_long_description(),
+    long_description_content_type='text/markdown',
     url='https://github.com/caleb531/cidr-brewer',
     author='Caleb Evans',
     author_email='caleb@calebevans.me',
